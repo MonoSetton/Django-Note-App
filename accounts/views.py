3b4951e307ba3949ce64c1fe8d54c3b8bd623a86
MonoSetton <maksymiliankloch@gmail.com>
2024-02-10 14:42:57 +0100

    Finished

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from notes.models import Note
 
